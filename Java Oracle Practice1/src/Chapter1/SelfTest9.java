80445497a354eb1503fce282b099d7f2e8df8d73
Achilles Ecos <Achilles Ecos@personal-pc>
2018-01-27 21:50:38 -0500

    09

---
+++ Java Oracle Practice1/src/Chapter1/SelfTest9.java
@@ -0,0 +1,22 @@
+package Chapter1;
+
+public class SelfTest9 {
+
+	
+	
+	public SelfTest9() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		double earthWeight;
+		double moonWeight;
+		
+		earthWeight = 150;
+		moonWeight = earthWeight * .17;
+		
+		System.out.println(moonWeight);
+
+	}
+
+}
