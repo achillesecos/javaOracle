20c6fe1b14b917fc62d52c1dc6be83160d83d189
Achilles Ecos <Achilles Ecos@personal-pc>
2018-02-04 17:42:00 -0500

    12ewdq

--- Java Oracle Practice1/src/Chapter3/Help.java
+++ Java Oracle Practice1/src/Chapter3/Help.java
@@ -25,7 +25,22 @@ public class Help {
 			System.out.println("The if:\n");
 			System.out.println("if(condition) statement;");
 			System.out.println("else statement;");
+			break;
+			
+		case '2':
+			System.out.println("The switch: \n");
+			System.out.println("switch(expression) {");
+			System.out.println("  case constant:");
+			System.out.println("    statement sequence");
+			System.out.println("    break;");
+			System.out.println("  // ...");
+			System.out.println("}");
+			break;
+			
+		default:
+			System.out.print("Selection not found.");
 		}
+		
 		
 		
 
